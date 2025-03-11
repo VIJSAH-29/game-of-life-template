83ad7906353c77e77a12a774fed21d61a6123d84
VIJSAH-29 <ishaan.vijaywargiya@gmail.com>
2025-03-11 01:54:18 +0000

    Commit 3

--- LifeGame.java
+++ LifeGame.java
@@ -26,63 +26,52 @@ public class LifeGame implements Grid {
         }
     }
 
-    // Run the simulation for a number of turns
-    public void run(int turns) {
-        // call step the number of times requested
-    }
+    // Step the simulation forward one generation.
+    public void evolve() {
+        display();
+        int[][] newGrid = new int[grid.length][grid[0].length];
 
-    // Step the simulation forward one turn.
-    public void step()
-    {
-        print();
-        // Update the game board, store a 1 if the cell is alive and a 0 otherwise.
-    }
-
-
-    public int countNeighbors(int x, int y) {
-        int count = 0;
-        // count the number of neighbors the cell has
-        // use the get(x,y) method to read any board state you need.
-        return count;
-    }
-
-    // Get a value from the board with "wrap around"
-    // Locations outside the board will loop back into the board.
-    // Ex: -1 will read board.length-1
-    public int get(int x, int y) {
-        int xLimit = board.length;
-        int yLimit= board[0].length;
-        return board[(x+xLimit)%xLimit][(y+yLimit)%yLimit];
-    }
-
-    // Test helper to get the whole board state
-    public int[][] get()
-    {
-        return board;
-    }
-
-    // Test helper to print the current state
-    public void print(){
-        // Print the header
-        System.out.print("\n ");
-        for (int y = 0; y < board[0].length; y++) {
-            System.out.print(y%10 + " ");
-        }
-
-        for (int x = 0; x < board.length; x++) {
-            System.out.print("\n" + x%10);
-            for (int y=0; y<board[x].length; y++)
-            {
-                if (board[x][y] == 1)
-                {
-                    System.out.print("⬛");
-                }
-                else
-                {
-                    System.out.print("⬜");
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                int neighbors = countAliveNeighbors(i, j);
+                if (grid[i][j] == 1) {
+                    // Alive cell rules
+                    if (neighbors < 2 || neighbors > 3) {
+                        newGrid[i][j] = 0; // Dies
+                    } else {
+                        newGrid[i][j] = 1; // Stays alive
+                    }
+                } else {
+                    // Dead cell rule
+                    if (neighbors == 3) {
+                        newGrid[i][j] = 1; // Becomes alive
+                    }
                 }
             }
         }
-        System.out.println();
+        grid = newGrid;
     }
-}
+
+    public int countAliveNeighbors(int row, int col) {
+        int count = 0;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (!(i == 0 && j == 0)) {
+                    count += getCell(row + i, col + j);
+                }
+            }
+        }
+        return count;
+    }
+
+    // Get a value from the grid with "wrap around"
+    public int getCell(int row, int col) {
+        int rowLimit = grid.length;
+        int colLimit = grid[0].length;
+        return grid[(row + rowLimit) % rowLimit][(col + colLimit) % colLimit];
+    }
+
+    // Test helper to get the whole grid state
+    public int[][] getGrid() {
+        return grid;
+    }
